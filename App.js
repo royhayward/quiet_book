a857794a60eae99d1743834e6d5145320252d68b
Roy Hayward <roy@ponderize.me>
2022-11-01 10:09:22 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [enteredPictureList, setPictureList] = useState('');
+  const [pictsJesus, setPictsJesus] = useState([]); 
+
+  function showPictures(listName){
+    console.log(listName);
+    setPictsJesus((currentPictures) => [
+      ...currentPictures,
+      pictsJesus
+    ]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Shh! This is a quiet app.</Text>
+      </View>
+      
+      <Button title='Pictures of Jesus' onPress={showPictures("Jesus")} />
       <StatusBar style="auto" />
     </View>
   );
